5f8289511c6864254c7c4e7609455ffe18de9886
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2018-02-08 21:48:38 +0100

    Initial commit

---
+++ Assets/Testing.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour 
+{
+    //------------------------------
+    // Unity methods
+    //------------------------------
+    private void Awake() 
+	{
+	}
+
+	private void Start() 
+	{
+	}
+	
+	private void Update() 
+	{
+        if(Input.GetKeyDown(KeyCode.F1))
+        {
+            Debug.Log("1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            Debug.LogWarning("xD");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            Debug.LogError(":D");
+        }
+    }
+
+    //------------------------------
+    // public methods
+    //------------------------------
+
+    //------------------------------
+    // private methods
+    //------------------------------
+
+    //------------------------------
+    // coroutines
+    //------------------------------
+}
