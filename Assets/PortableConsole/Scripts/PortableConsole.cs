58aaa68ee9424102ce5e1d40e3e3f46d655fa969
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2018-02-10 16:01:11 +0100

    logging PoC

---
+++ Assets/PortableConsole/Scripts/PortableConsole.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace PortableConsole
+{
+    public class PortableConsole : MonoBehaviour
+    {
+        //------------------------------
+        // Unity methods
+        //------------------------------
+        private void Awake()
+        {
+            Application.logMessageReceived += LogMessageReceived;
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+
+        //------------------------------
+        // public methods
+        //------------------------------
+
+        //------------------------------
+        // private methods
+        //------------------------------
+        private void LogMessageReceived(string condition, string stackTrace, LogType type)
+        {
+            Debug.LogFormat("Logged type: {0}, condition: {1}", type.ToString(), condition);
+        }
+
+        //------------------------------
+        // coroutines
+        //------------------------------
+    }
+}
---
+++ Assets/PortableConsole/Scripts/PortableConsolePresenter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PortableConsole
+{
+    [RequireComponent(typeof(PortableConsole))]
+    public class PortableConsolePresenter: MonoBehaviour
+    {
+        private PortableConsole _portableConsole;
+        //------------------------------
+        // Unity methods
+        //------------------------------
+        private void Awake()
+        {
+            _portableConsole = GetComponent<PortableConsole>();
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+
+        //------------------------------
+        // public methods
+        //------------------------------
+
+        //------------------------------
+        // private methods
+        //------------------------------
+
+        //------------------------------
+        // coroutines
+        //------------------------------
+    }
+}
