edb4633aba6ecd8d1da935338baf7a729c498b1c
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2018-02-12 18:56:24 +0100

    Added internal log type

---
+++ Assets/PortableConsole/Scripts/PortableConsoleLog.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace PortableConsole
+{
+    public class PortableConsoleLog
+    {
+        public LogType LogType { get; private set; }
+
+        public string Name { get; set; }
+        public string Caster { get; set; }
+
+        public string Details { get; set; }
+        //------------------------------
+        // public methods
+        //------------------------------
+        public PortableConsoleLog(LogType type, string name, string details)
+        {
+            LogType = type;
+            Name = name;
+            Details = details;
+            Caster = details.Trim(' ');
+        }
+
+        //------------------------------
+        // private methods
+        //------------------------------
+
+        //------------------------------
+        // coroutines
+        //------------------------------
+    }
+}
